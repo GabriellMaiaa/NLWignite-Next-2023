6a6d10251093c1b5800f52cff05e941da963d6f7
Gabriel Maia <gribeiromaia736@gmail.com>
2023-05-18 12:59:20 -0300

    finishing one part of web

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">{/*Tem que ser dividido no meio em duas secoes */}
       {/*1 Parte- Left*/}
-      <div className="flex flex-col items-start justify-between pr-28 pl-28 pt-16 pb-16 relative">
-        <div className="absulute right-0 top-1/2 h-[288px] w-[526px]"></div>
+      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10">
+        {/*Efeito BLUR da cor Roxa*/}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] 
+        -translate-y-1/2 translate-x-1/2 bg-purple-700 opacity-50 rounded-full blur-full"></div>
+        {/*Stripes - Linhaszinhas no meio da aplicação*/}
+        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes  pr-2"/>
+
       </div>
 
       {/*2 Parte- Right*/}
